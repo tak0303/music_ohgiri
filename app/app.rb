537f64e9d30badd820f6be23f1d90805ca02dbc3
Takashi Nakagawa <tak1240@gmail.com>
2013-01-31 16:43:16 +0900

    delete authenticate filter, and add some routing

--- app/app.rb
+++ app/app.rb
@@ -9,7 +9,7 @@ class MusicOhgiri < Padrino::Application
 
   enable :sessions
 
-  # register Padrino::Admin::AccessControl
+  register Padrino::Admin::AccessControl
 
   register Padrino::Sprockets
   sprockets  # :url => 'assets', :root => app.root
@@ -18,70 +18,19 @@ class MusicOhgiri < Padrino::Application
     provider :facebook, '368370446603454', 'd5333763b294efde84e60060815e506b'
   end
 
-  # set :login_page, '/'
+  get :root, :map => '/' do
+    render 'index/index'
+  end
 
-  # access_control.roles_for :any do |role|
-  #   role.protect '/index'
-  #   role.protect 'admin'
-  # end
+  get :auth, :map => '/auth/:provider/callback' do
+    auth = request.env['omniauth.auth']
+    account = Account.find_by_provider_and_uid(auth['provider'], auth['uid']) ||
+      Account.create_with_omniauth(auth)
+    set_current_account(account)
+    redirect '/'
+  end
 
-  # access_control.roles_for :users do |role|
-  #   role.allow '/index'
-  # end
-
-  # get '/' do
-  #   render 'index/login'
-  # end
-
-  ##
-  # Caching support
-  #
-  # register Padrino::Cache
-  # enable :caching
-  #
-  # You can customize caching store engines:
-  #
-  #   set :cache, Padrino::Cache::Store::Memcache.new(::Memcached.new('127.0.0.1:11211', :exception_retry_limit => 1))
-  #   set :cache, Padrino::Cache::Store::Memcache.new(::Dalli::Client.new('127.0.0.1:11211', :exception_retry_limit => 1))
-  #   set :cache, Padrino::Cache::Store::Redis.new(::Redis.new(:host => '127.0.0.1', :port => 6379, :db => 0))
-  #   set :cache, Padrino::Cache::Store::Memory.new(50)
-  #   set :cache, Padrino::Cache::Store::File.new(Padrino.root('tmp', app_name.to_s, 'cache')) # default choice
-  #
-
-  ##
-  # Application configuration options
-  #
-  # set :raise_errors, true       # Raise exceptions (will stop application) (default for test)
-  # set :dump_errors, true        # Exception backtraces are written to STDERR (default for production/development)
-  # set :show_exceptions, true    # Shows a stack trace in browser (default for development)
-  # set :logging, true            # Logging in STDOUT for development and file for production (default only for development)
-  # set :public_folder, "foo/bar" # Location for static assets (default root/public)
-  # set :reload, false            # Reload application files (default in development)
-  # set :default_builder, "foo"   # Set a custom form builder (default 'StandardFormBuilder')
-  # set :locale_path, "bar"       # Set path for I18n translations (default your_app/locales)
-  # disable :sessions             # Disabled sessions by default (enable if needed)
-  # disable :flash                # Disables sinatra-flash (enabled by default if Sinatra::Flash is defined)
-  # layout  :my_layout            # Layout can be in views/layouts/foo.ext or views/foo.ext (default :application)
-  #
-
-  ##
-  # You can configure for a specified environment like:
-  #
-  #   configure :development do
-  #     set :foo, :bar
-  #     disable :asset_stamp # no asset timestamping for dev
-  #   end
-  #
-
-  ##
-  # You can manage errors like:
-  #
-  #   error 404 do
-  #     render 'errors/404'
-  #   end
-  #
-  #   error 505 do
-  #     render 'errors/505'
-  #   end
-  #
+  error 404 do
+    render('errors/response')
+  end
 end
