e615d3b0aaf1e1337c461320f5a635e6db37a45a
Takashi Nakagawa <tak1240@gmail.com>
2013-01-31 16:44:29 +0900

    delete some routing and add login check filter

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,18 +1,14 @@
 MusicOhgiri.controllers :index do
-  get :new do
+  before do
+    redirect '/' if current_account.blank?
+  end
+
+  get :new, :map => '/new' do
     render 'index/new'
   end
 
-  get :destroy do
+  get :destroy, :map => '/logout' do
     set_current_account(nil)
-    redirect url(:index)
-  end
-
-  get :auth, :map => '/auth/:provider/callback' do
-    auth = request.env['omniauth.auth']
-    account = Account.find_by_provider_and_uid(auth['provider'], auth['uid']) ||
-      Account.create_with_omniauth(auth)
-    set_current_account(account)
-    redirect 'http://' + request.env['HTTP_HOST'] + url(:profile)
+    redirect url(:root)
   end
 end
