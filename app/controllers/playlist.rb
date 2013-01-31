e4742a675804efc3d7ea51acc5b1b4f578255147
Takashi Nakagawa <tak1240@gmail.com>
2013-01-31 16:44:06 +0900

    add methods for making playlist

--- app/controllers/playlist.rb
+++ app/controllers/playlist.rb
@@ -1,5 +1,17 @@
+require 'json'
 MusicOhgiri.controllers :playlist do
-  get ':playlist_id/add/:video_id', :provides => [:json, :xml] do
-
+  get ':topic_id/add/:video_id', :provides => [:json, :xml] do
+    p_list = Playlist.find_by_user_id_and_topic_id current_account.id, params[:topic_id]
+    unless p_list.present?
+      p_list = Playlist.new({
+        topic_id: params[:topic_id],
+        user_id: current_account.id
+      }).save!
+    end
+    Video.find_or_create_by_video_id_and_playlist_id({
+      video_id: params[:video_id],
+      playlist_id: p_list.id
+    }).save
+    return JSON.generate([true])
   end
 end
