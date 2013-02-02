8d7cb5ba7630d6b2f23147a728875984542945cb
Takashi Nakagawa <tak1240@gmail.com>
2013-02-02 22:00:46 +0900

    fixed account info

--- models/account.rb
+++ models/account.rb
@@ -1,9 +1,10 @@
 class Account < ActiveRecord::Base
+  has_many :playlists
   def self.create_with_omniauth(auth)
     create!(
       provider: auth['provider'],
       uid: auth['uid'],
-      name: auth['name'],
+      name: auth['info']['name'],
       role: 'users'
     )
   end
