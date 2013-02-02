79f82a594b5f6219434f971d567da32629548c08
Takashi Nakagawa <tak1240@gmail.com>
2013-02-02 21:59:32 +0900

    add show, show_by_topic method

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,6 +3,17 @@ MusicOhgiri.controllers :index do
     redirect '/' if current_account.blank?
   end
 
+  get :show, :map => '/show/:playlist_id' do
+    @list = Playlist.find params[:playlist_id]
+    render 'index/show'
+  end
+
+  get :show_by_topic, :map => '/show/topic/:topic_id' do
+    @topic_id = params[:topic_id]
+    @list_by_topic = Playlist.find_all_by_topic_id @topic_id
+    render 'index/show_by_topic'
+  end
+
   get :new, :map => '/new' do
     render 'index/new'
   end
