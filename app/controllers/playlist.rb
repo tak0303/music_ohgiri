4dee61e2c2fe50d8851ee58981c0435d467ccad2
Takashi Nakagawa <tak1240@gmail.com>
2013-02-02 21:59:51 +0900

    move logic to model

--- app/controllers/playlist.rb
+++ app/controllers/playlist.rb
@@ -1,17 +1,12 @@
 require 'json'
 MusicOhgiri.controllers :playlist do
   get ':topic_id/add/:video_id', :provides => [:json, :xml] do
-    p_list = Playlist.find_by_user_id_and_topic_id current_account.id, params[:topic_id]
-    unless p_list.present?
-      p_list = Playlist.new({
-        topic_id: params[:topic_id],
-        user_id: current_account.id
-      }).save!
-    end
-    Video.find_or_create_by_video_id_and_playlist_id({
-      video_id: params[:video_id],
-      playlist_id: p_list.id
-    }).save
+    Playlist.add_video_info_by_topic params, current_account
+    return JSON.generate([true])
+  end
+
+  get ':topic_id/remove/:video_id', :provides => [:json, :xml] do
+    Playlist.remove_video_info_by_topic params, current_account
     return JSON.generate([true])
   end
 end
--- models/playlist.rb
+++ models/playlist.rb
@@ -1,3 +1,38 @@
 class Playlist < ActiveRecord::Base
   has_many :videos
+  belongs_to :account
+
+  class << self
+    def add_video_info_by_topic info, current_account
+      begin
+        p_list = Playlist.find_by_user_id_and_topic_id current_account.id, info[:topic_id]
+        unless p_list.present?
+          Playlist.new({
+            topic_id: info[:topic_id],
+            user_id: current_account.id
+          }).save
+          p_list = Playlist.find_by_user_id_and_topic_id current_account.id, info[:topic_id]
+        end
+        video_info = YouTubeIt::Client.new.video_by info[:video_id]
+        Video.find_or_create_by_video_id_and_playlist_id({
+          video_id: info[:video_id],
+          playlist_id: p_list.id,
+          title: video_info.title,
+          thumbnail_url: video_info.thumbnails[1].url,
+          embed_url: video_info.embed_url
+        }).save
+      rescue => e
+        raise e
+      end
+    end
+
+    def remove_video_info_by_topic info, current_account
+      p_list = Playlist.find_by_user_id_and_topic_id current_account.id, info[:topic_id]
+      raise 'cant find playlist' unless p_list.present?
+      Video.find_or_create_by_video_id_and_playlist_id({
+        video_id: info[:video_id],
+        playlist_id: p_list.id
+      }).delete
+    end
+  end
 end
