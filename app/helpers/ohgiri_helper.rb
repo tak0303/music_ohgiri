a470800d461aa8680c70cf657671aeb2e2a636c7
Takashi Nakagawa <tak1240@gmail.com>
2013-02-02 22:00:00 +0900

    add helper for view

--- app/helpers/ohgiri_helper.rb
+++ app/helpers/ohgiri_helper.rb
@@ -12,4 +12,24 @@ MusicOhgiri.helpers do
     response.merge! ingredient if !ingredient.nil? and ingredient.is_a?(Hash)
     return build_response(response)
   end
+
+  def get_playlist_by_topic
+    list = Playlist.find_by_user_id_and_topic_id(current_account.id,TodaysTopic.new.get.id)
+    return false if list.blank?
+    list
+  end
+
+  def get_all_playlist_by_topic
+    lists = Array.new
+    Account.find(:all).each do |user|
+    list = Playlist.find_by_user_id_and_topic_id(current_account.id,TodaysTopic.new.get.id)
+    lists << list unless list.blank?
+    end
+    lists
+  end
+
+  def video_info video_id
+    @client ||= YouTubeIt::Client.new
+    @client.video_by video_id
+  end
 end
